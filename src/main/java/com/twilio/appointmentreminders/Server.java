d4609a8f1b2f489136821b197ae269a2251c721c
Mario Celi <mceli@stackbuilders.com>
2015-07-27 17:03:28 -0500

    Initial form implementation

--- src/main/java/com/twilio/appointmentreminders/Server.java
+++ src/main/java/com/twilio/appointmentreminders/Server.java
@@ -1,18 +1,25 @@
 package com.twilio.appointmentreminders;
 
-import static spark.Spark.get;
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
+
 import java.util.HashMap;
 import java.util.Map;
 
+import static spark.Spark.get;
+
 public class Server {
 
-  // Bootstrap our Java application
-  public static void main(String[] args) {
-    Map map = new HashMap();
-    map.put("name", "Mario");
+    // Bootstrap our Java application
+    public static void main(String[] args) {
+        get("/:name", (request, response) -> {
+            Map map = new HashMap();
+            map.put("name", request.params(":name"));
+            return new ModelAndView(map, "index.mustache");
+        }, new MustacheTemplateEngine());
 
-    get("/", (rq, rs) -> new ModelAndView(map, "index.mustache"), new MustacheTemplateEngine());
-  }
+        get("/hello/:name", (request, response) -> {
+            return "Hello: " + request.params(":name");
+        });
+    }
 }
